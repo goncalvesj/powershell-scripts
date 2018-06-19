59bad2a6df84f28a1dcf1322170b54f3eaa192f7
Joao Goncalves <joao.goncalves@oireachtas.ie>
2018-06-19 16:32:26 +0100

    Service principals AD group script

---
+++ PsScriptsProject/AddAzureServicePrincipalToADGroup.ps1
@@ -0,0 +1,23 @@
+﻿#
+# Script to add a bunch of service principals that belong to azure web apps with managed service identity to an Azure AD group
+# The group can then be used in a key vault to grant access to all web apps in one go
+#
+
+
+#Log in to Azure
+#Login-AzureRmAccount
+
+#Creates Azure AD Group
+$Group = New-AzureADGroup -Description "Service Principals" -DisplayName "App Service Principals" -MailEnabled $false -SecurityEnabled $true -MailNickName "AppServicePrincipals"
+
+#Gets All Service Principals that contain "dev" in their name
+$List = Get-AzureRmADServicePrincipal | Where-Object { $_.DisplayName -match "dev" }
+
+$List | ForEach-Object { 
+
+	Write-Output $_.DisplayName
+
+	#Add Service Principal to AD Group
+	Add-AzureADGroupMember -ObjectId $Group.ObjectId -RefObjectId $_.Id
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # powershell-scripts
+
+repo for powershell scripts that I find useful or were useful at some point :)
